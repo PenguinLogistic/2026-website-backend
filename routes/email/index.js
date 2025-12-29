833b9f31e064f776a642d8a41c0c01b3514a6dfc
PenguinLogistic <ryan.shiuhong.fung@gmail.com>
2025-12-29 10:48:56 -0500

    email route base

--- routes/email/index.js
+++ routes/email/index.js
@@ -2,7 +2,7 @@
 
 module.exports = async function (fastify, opts) {
   fastify.get("/", async (req, res) => {
-    const data = { status: "you've hit the skills route!" };
+    const data = { status: "you've hit the email route!" };
     console.log("Sending response:", JSON.stringify(data));
     res.header("Content-Type", "application/json");
     res.send(data);
