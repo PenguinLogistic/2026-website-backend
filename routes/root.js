bdf73b733fd85a9e56eca453d8a366d438c7c541
PenguinLogistic <ryan.shiuhong.fung@gmail.com>
2026-01-02 12:20:23 -0500

    temp test route

--- routes/root.js
+++ routes/root.js
@@ -8,4 +8,8 @@ module.exports = async function (fastify, opts) {
   fastify.get("/health", async (req, res) => {
     return { status: "ok" };
   });
+
+  fastify.get("/test-env", async () => {
+    return { DATABASE_URL: process.env.DATABASE_URL };
+  });
 };
