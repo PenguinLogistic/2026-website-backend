19b45d2ed7bc730e07ae723245add1e227f219ec
PenguinLogistic <ryan.shiuhong.fung@gmail.com>
2026-01-02 11:39:47 -0500

    use connectionString to better suit railway

--- database/db.js
+++ database/db.js
@@ -1,10 +1,11 @@
 import { Pool } from "pg";
 
 export const pool = new Pool({
-  host: process.env.DATABASE_URL,
-  port: Number(process.env.DB_PORT),
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  // host: process.env.DB_HOST,
+  // port: Number(process.env.DB_PORT),
+  // user: process.env.DB_USER,
+  // password: process.env.DB_PASSWORD,
+  // database: process.env.DB_NAME,
+  connectionString: process.env.DATABASE_URL,
   ssl: process.env.ENV === "production" ? { rejectUnauthorized: false } : false,
 });
