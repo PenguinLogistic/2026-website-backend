cce73664ce6e61bc234a20480cfa82da637d09db
PenguinLogistic <ryan.shiuhong.fung@gmail.com>
2026-01-02 12:09:28 -0500

    enforce db ssl for railway

--- database/db.js
+++ database/db.js
@@ -2,5 +2,5 @@ import { Pool } from "pg";
 
 export const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: process.env.ENV === "production" ? { rejectUnauthorized: false } : false,
+  ssl: { rejectUnauthorized: false }, //comment out in dev
 });
